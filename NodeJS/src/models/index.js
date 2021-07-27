203b78dc4aae655330c0056538b7549a6d00b6f7
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-07-27 07:56:17 +0200

    feat(init): add models collector

---
+++ NodeJS/src/models/index.js
@@ -0,0 +1,7 @@
+import Token from './token.model';
+import User from './user.model';
+import Child from './child.model';
+import Camera from './camera.model';
+import ErrorStack from './error.model';
+
+export { Token, User, Child, Camera, ErrorStack };
