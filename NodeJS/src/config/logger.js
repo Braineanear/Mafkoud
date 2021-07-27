08a66ad77fcfc1da6074715fcfd30af8d43ca4b9
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-07-27 07:45:18 +0200

    feat(config): add logger

---
+++ NodeJS/src/config/logger.js
@@ -0,0 +1,28 @@
+// Packages
+import winston from 'winston';
+
+// Error Format
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+// Create Logger
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    winston.format.colorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error']
+    })
+  ]
+});
+
+export default logger;
