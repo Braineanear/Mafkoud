6da91ece5dcd98a5e611160aa36bdd03c67d5468
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-07-27 07:53:03 +0200

    feat(middleware): add rate limiter middleware for handling number of requests per ip on the production stage

---
+++ NodeJS/src/middlewares/rateLimiter.js
@@ -0,0 +1,10 @@
+// Packages
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  skipSuccessfulRequests: true
+});
+
+export default limiter;
